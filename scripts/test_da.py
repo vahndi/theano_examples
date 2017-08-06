0f3d9f37d09065393eca1b062ed8500d0ad11d9b
Vahndi Minah <vahndi.minah@gmail.com>
2017-08-06 19:04:42 -0400

    initial commit

---
+++ scripts/test_da.py
@@ -0,0 +1,195 @@
+"""
+ This tutorial introduces denoising auto-encoders (dA) using Theano.
+
+ Denoising autoencoders are the building blocks for SdA.
+ They are based on auto-encoders as the ones used in Bengio et al. 2007.
+ An autoencoder takes an input x and first maps it to a hidden representation
+ y = f_{\theta}(x) = s(Wx+b), parameterized by \theta={W,b}. The resulting
+ latent representation y is then mapped back to a "reconstructed" vector
+ z \in [0,1]^d in input space z = g_{\theta'}(y) = s(W'y + b').  The weight
+ matrix W' can optionally be constrained such that W' = W^T, in which case
+ the autoencoder is said to have tied weights. The network is trained such
+ that to minimize the reconstruction error (the error between x and z).
+
+ For the denosing autoencoder, during training, first x is corrupted into
+ \tilde{x}, where \tilde{x} is a partially destroyed version of x by means
+ of a stochastic mapping. Afterwards y is computed as before (using
+ \tilde{x}), y = s(W\tilde{x} + b) and z as s(W'y + b'). The reconstruction
+ error is now measured between z and the uncorrupted input x, which is
+ computed as the cross-entropy :
+      - \sum_{k=1}^d[ x_k \log z_k + (1-x_k) \log( 1-z_k)]
+
+
+ References :
+   - P. Vincent, H. Larochelle, Y. Bengio, P.A. Manzagol: Extracting and
+   Composing Robust Features with Denoising Autoencoders, ICML'08, 1096-1103,
+   2008
+   - Y. Bengio, P. Lamblin, D. Popovici, H. Larochelle: Greedy Layer-Wise
+   Training of Deep Networks, Advances in Neural Information Processing
+   Systems 19, 2007
+
+"""
+from utils import tile_raster_images
+
+try:
+    import PIL.Image as Image
+except ImportError:
+    import Image
+
+import numpy
+import os
+import sys
+import theano
+import theano.tensor as T
+from theano.tensor.shared_randomstreams import RandomStreams
+import timeit
+
+from classes.denoising_autoencoder import DenoisingAutoEncoder
+from io import load_data
+
+
+def test_dA(learning_rate=0.1, training_epochs=15,
+            dataset='mnist.pkl.gz',
+            batch_size=20, output_folder='dA_plots'):
+
+    """
+    This demo is tested on MNIST
+
+    :type learning_rate: float
+    :param learning_rate: learning rate used for training the DeNosing
+                          AutoEncoder
+
+    :type training_epochs: int
+    :param training_epochs: number of epochs used for training
+
+    :type dataset: string
+    :param dataset: path to the picked dataset
+
+    """
+    datasets = load_data(dataset)
+    train_set_x, train_set_y = datasets[0]
+    # compute number of mini-batches for training, validation and testing
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
+    # allocate symbolic variables for the data
+    index = T.lscalar()    # index to a [mini]batch
+    x = T.matrix('x')  # the data is presented as rasterized images
+
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
+    os.chdir(output_folder)
+
+    ####################################
+    # BUILDING THE MODEL NO CORRUPTION #
+    ####################################
+
+    rng = numpy.random.RandomState(123)
+    theano_rng = RandomStreams(rng.randint(2 ** 30))
+
+    da = DenoisingAutoEncoder(
+        numpy_rng=rng, theano_rng=theano_rng,
+        input=x, n_visible=28 * 28, n_hidden=500
+    )
+
+    cost, updates = da.get_cost_updates(
+        corruption_level=0.,
+        learning_rate=learning_rate
+    )
+
+    train_da = theano.function(
+        [index],
+        cost,
+        updates=updates,
+        givens={
+            x: train_set_x[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+
+    start_time = timeit.default_timer()
+
+    ############
+    # TRAINING #
+    ############
+
+    # go through training epochs
+    for epoch in range(training_epochs):
+        # go through trainng set
+        c = []
+        for batch_index in range(int(n_train_batches)):
+            c.append(train_da(batch_index))
+
+        print('Training epoch %d, cost ' % epoch, numpy.mean(c))
+
+    end_time = timeit.default_timer()
+
+    training_time = (end_time - start_time)
+
+    print(('The no corruption code for file %s ran for %.2fm' %
+           (os.path.split(__file__)[1], training_time / 60.)),
+          file=sys.stderr)
+    image = Image.fromarray(
+        tile_raster_images(X=da.W.get_value(borrow=True).T,
+                           img_shape=(28, 28), tile_shape=(10, 10),
+                           tile_spacing=(1, 1)))
+    image.save('filters_corruption_0.png')
+
+    #####################################
+    # BUILDING THE MODEL CORRUPTION 30% #
+    #####################################
+
+    rng = numpy.random.RandomState(123)
+    theano_rng = RandomStreams(rng.randint(2 ** 30))
+
+    da = DenoisingAutoEncoder(
+        numpy_rng=rng, theano_rng=theano_rng,
+        input=x, n_visible=28 * 28, n_hidden=500
+    )
+
+    cost, updates = da.get_cost_updates(
+        corruption_level=0.3,
+        learning_rate=learning_rate
+    )
+
+    train_da = theano.function(
+        [index],
+        cost,
+        updates=updates,
+        givens={
+            x: train_set_x[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+
+    start_time = timeit.default_timer()
+
+    ############
+    # TRAINING #
+    ############
+
+    # go through training epochs
+    for epoch in range(training_epochs):
+        # go through training set
+        c = []
+        for batch_index in range(int(n_train_batches)):
+            c.append(train_da(batch_index))
+
+        print('Training epoch %d, cost ' % epoch, numpy.mean(c))
+
+    end_time = timeit.default_timer()
+
+    training_time = (end_time - start_time)
+
+    print(('The 30%% corruption code for file %s ran for %.2fm'
+           % (os.path.split(__file__)[1], training_time / 60.)),
+          file=sys.stderr)
+
+    image = Image.fromarray(tile_raster_images(
+        X=da.W.get_value(borrow=True).T,
+        img_shape=(28, 28), tile_shape=(10, 10),
+        tile_spacing=(1, 1)))
+    image.save('filters_corruption_30.png')
+
+    os.chdir('../')
+
+
+if __name__ == '__main__':
+
+    test_dA()
---
+++ scripts/test_dbn.py
@@ -0,0 +1,162 @@
+import os
+import numpy
+import sys
+import timeit
+
+from classes.deep_belief_network import DeepBeliefNetwork
+from io import load_data
+
+
+def test_DBN(fine_tune_lr: float=0.1, pre_training_epochs: int=100,
+             pre_train_lr: float=0.01, k: int=1, training_epochs: int=1000,
+             dataset: str='mnist.pkl.gz', batch_size: int=10):
+    """
+    Demonstrates how to train and test a Deep Belief Network.
+    This is demonstrated on MNIST.
+
+    :param fine_tune_lr: learning rate used in the finetune stage
+    :param pre_training_epochs: number of epoch to do pretraining
+    :param pre_train_lr: learning rate to be used during pre-training
+    :param k: number of Gibbs steps in CD/PCD
+    :param training_epochs: maximal number of iterations ot run the optimizer
+    :param dataset: path to the pickled dataset
+    :param batch_size: the size of a mini-batch
+    """
+
+    datasets = load_data(dataset)
+
+    train_set_x, train_set_y = datasets[0]
+    valid_set_x, valid_set_y = datasets[1]
+    test_set_x, test_set_y = datasets[2]
+
+    # compute number of minibatches for training, validation and testing
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
+
+    # numpy random generator
+    numpy_rng = numpy.random.RandomState(123)
+    print('... building the model')
+    # construct the Deep Belief Network
+    dbn = DeepBeliefNetwork(
+        numpy_rng=numpy_rng, n_ins=28 * 28,
+        hidden_layers_sizes=[1000, 1000, 1000],
+        n_outs=10
+    )
+
+    #########################
+    # PRETRAINING THE MODEL #
+    #########################
+    print('... getting the pre-training functions')
+    pre_training_fns = dbn.pretraining_functions(
+        train_set_x=train_set_x, batch_size=batch_size, k=k
+    )
+
+    print('... pre-training the model')
+    start_time = timeit.default_timer()
+    ## Pre-train layer-wise
+    for i in range(dbn.n_layers):
+        # go through pretraining epochs
+        for epoch in range(pre_training_epochs):
+            # go through the training set
+            c = []
+            for batch_index in range(int(n_train_batches)):
+                c.append(pre_training_fns[i](index=batch_index,
+                                             lr=pre_train_lr))
+            print('Pre-training layer %i, epoch %d, cost ' % (i, epoch), end=' ')
+            print(numpy.mean(c))
+
+    end_time = timeit.default_timer()
+    print(('The pre-training code for file ' +
+                          os.path.split(__file__)[1] +
+                          ' ran for %.2fm' % ((end_time - start_time) / 60.)), file=sys.stderr)
+
+    ########################
+    # FINETUNING THE MODEL #
+    ########################
+
+    # get the training, validation and testing function for the model
+    print('... getting the finetuning functions')
+    train_fn, validate_model, test_model = dbn.build_finetune_functions(
+        datasets=datasets, batch_size=batch_size, learning_rate=fine_tune_lr
+    )
+
+    print('... finetuning the model')
+    # early-stopping parameters
+    patience = 4 * n_train_batches  # look as this many examples regardless
+    patience_increase = 2.    # wait this much longer when a new best is found
+    improvement_threshold = 0.995  # a relative improvement of this much is considered significant
+    validation_frequency = min(n_train_batches, patience / 2)
+                                  # go through this many
+                                  # minibatches before checking the network
+                                  # on the validation set; in this case we
+                                  # check every epoch
+
+    best_validation_loss = numpy.inf
+    test_score = 0.
+    start_time = timeit.default_timer()
+
+    done_looping = False
+    epoch = 0
+
+    while (epoch < training_epochs) and (not done_looping):
+        epoch += 1
+        for mini_batch_index in range(int(n_train_batches)):
+            mini_batch_avg_cost = train_fn(mini_batch_index)
+            iter = (epoch - 1) * n_train_batches + mini_batch_index
+
+            if (iter + 1) % validation_frequency == 0:
+
+                validation_losses = validate_model()
+                this_validation_loss = numpy.mean(validation_losses)
+                print((
+                    'epoch %i, minibatch %i/%i, validation error %f %%'
+                    % (
+                        epoch,
+                        mini_batch_index + 1,
+                        n_train_batches,
+                        this_validation_loss * 100.
+                    )
+                ))
+
+                # if we got the best validation score until now
+                if this_validation_loss < best_validation_loss:
+
+                    #improve patience if loss improvement is good enough
+                    if (
+                        this_validation_loss < best_validation_loss *
+                        improvement_threshold
+                    ):
+                        patience = max(patience, iter * patience_increase)
+
+                    # save best validation score and iteration number
+                    best_validation_loss = this_validation_loss
+                    best_iter = iter
+
+                    # test it on the test set
+                    test_losses = test_model()
+                    test_score = numpy.mean(test_losses)
+                    print((('     epoch %i, minibatch %i/%i, test error of '
+                           'best model %f %%') %
+                          (epoch, mini_batch_index + 1, n_train_batches,
+                           test_score * 100.)))
+
+            if patience <= iter:
+                done_looping = True
+                break
+
+    end_time = timeit.default_timer()
+    print((
+        (
+            'Optimization complete with best validation score of %f %%, '
+            'obtained at iteration %i, '
+            'with test performance %f %%'
+        ) % (best_validation_loss * 100., best_iter + 1, test_score * 100.)
+    ))
+    print(('The fine tuning code for file ' +
+                          os.path.split(__file__)[1] +
+                          ' ran for %.2fm' % ((end_time - start_time)
+                                              / 60.)), file=sys.stderr)
+
+
+if __name__ == '__main__':
+
+    test_DBN()
